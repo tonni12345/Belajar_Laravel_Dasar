f35eb1593d8caf05c433bbb76bdffe4b7782b99b
Tonni Ramdani <tonniramdani@gmail.com>
2024-11-13 12:32:59 +0700

    tes Facades

---
+++ tests/Feature/FacadesTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class FacadesTest extends TestCase
+{
+    public function testConfig(){
+        // menggunakan helper function                                            
+        $firstName = config("contoh.author.firstName");
+
+        // menggunakan facades
+        $firstName1 = Config::get("contoh.author.firstName");
+
+        self::assertEquals($firstName, $firstName1);
+
+        var_dump(Config::all());
+    }
+}
