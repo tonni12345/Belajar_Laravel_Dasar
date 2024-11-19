2fb125d5ff6d324be1847769c383e778118c05e8
Tonni Ramdani <tonniramdani@gmail.com>
2024-11-19 07:37:26 +0700

    membuat view test

---
+++ tests/Feature/ViewTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ViewTest extends TestCase
+{
+    public function testView(){
+        $this->get('/hello')
+             ->assertSeeText('Hello Tonni');
+
+        $this->get('/hello-again')
+             ->assertSeeText('Hello Foo');
+
+    }
+}
