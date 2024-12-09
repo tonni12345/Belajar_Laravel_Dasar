deef08ed5a6b151efec83b82b906de145b98bd94
Tonni Ramdani <tonniramdani@gmail.com>
2024-12-09 09:14:27 +0700

    Controller

--- routes/web.php
+++ routes/web.php
@@ -78,3 +78,5 @@ Route::get('/produk/{id}', function($id){
 Route::get('/produk-redirect/{id}', function($id){
     return redirect()->route('product.detail', ['id' => $id]);
 });
+
+Route::get('/controller/hello/{name}', [\App\Http\Controllers\HelloController::class, 'hello']);
---
+++ tests/Feature/HelloControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HelloControllerTest extends TestCase
+{
+    public function testHello()
+    {
+        $this->get('/controller/hello/toni')->assertSeeText("Hallo toni");
+    }
+}
