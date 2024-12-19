38d98ca93749362222d531b418eec7a5ae4aee39
Tonni Ramdani <tonniramdani@gmail.com>
2024-12-19 19:37:23 +0700

    Error Reporter

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,7 +44,9 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
+            // var_dump($e);
+            // return false;
         });
+        
     }
 }
